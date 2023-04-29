9ed960c7f9e6807b6a2f63caeb360e24f29f559c
karam-zomlut <zomlutk@gmail.com>
2023-04-29 22:53:55 +0200

    Schemas

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err))
 
+/* 
+  SCHEMA TYPES:
+    1. String
+    2. Number
+    3. Date
+    4. Buffer : used for storing binary data
+    5. Boolean
+    6. ObjectID : used for assigning unit identifiers
+    7. Array
+*/
 
-
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  data: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
