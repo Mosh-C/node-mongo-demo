b4c5308b414f5a6e12a24c724959a6e675508109
karam-zomlut <zomlutk@gmail.com>
2023-04-30 22:25:12 +0300

    Logical Query Operators

--- src/index.js
+++ src/index.js
@@ -47,10 +47,15 @@ const getCourses = async () => {
   // in
   // nin  (not in)
 
+  // or
+  // and
+
   const courses = await Course
-  // .find({ price: { $gte: 10, $lte: 20 } })
-  // .find({ price: { $in: [10, 15, 20] } })
-    .find({ author: 'Mosh', isPublished: true, })
+    // .find({ price: { $gte: 10, $lte: 20 } })
+    // .find({ price: { $in: [10, 15, 20] } })
+    // .find()
+    // .or([{ author: 'Mosh' }, { isPublished: true }])
+    .find({ author: 'Mosh', isPublished: true })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 })
