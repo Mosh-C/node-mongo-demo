9b57de4f841bef2824144108ee9fadec4f592c42
karam-zomlut <zomlutk@gmail.com>
2023-05-02 17:59:31 +0300

    Delete comments

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/playground')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err))
 
-/* 
-  SCHEMA TYPES:
-    1. String
-    2. Number
-    3. Date
-    4. Buffer : used for storing binary data
-    5. Boolean
-    6. ObjectID : used for assigning unit identifiers
-    7. Array
-*/
-
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
@@ -36,38 +25,13 @@ const createCourse = async () => {
   const result = await course.save();
   console.log(result);
 };
-// createCourse();
 
 const getCourses = async () => {
-  // eq   (equal)
-  // ne   (not equal)
-  // gt   (grather than)
-  // gte  (grather than or equal)
-  // lt   (less than)
-  // lte  (less than or equal)
-  // in
-  // nin  (not in)
-
-  // or
-  // and
 
   const pageNumber = 2;
   const pageSize = 10;
 
   const courses = await Course
-    // .find({ price: { $gte: 10, $lte: 20 } })
-    // .find({ price: { $in: [10, 15, 20] } })
-    // .find()
-    // .or([{ author: 'Mosh' }, { isPublished: true }])
-
-    // Starts with Mosh
-    // .find({ author: /^Mosh/ })
-
-    // Ends with Hamedani
-    // .find({ author: /Mosh$/i })
-
-    // Contains Mosh
-    // .find({ author: /.*Mosh.*/ })
 
     .find({ author: 'Mosh', isPublished: true })
     .skip((pageNumber - 1) * pageSize)
@@ -76,8 +40,6 @@ const getCourses = async () => {
     .select({ name: 1, tags: 1 })
   console.log(courses);
 };
-
-// getCourses();
 
 const updateCourse = async (id) => {
   try {
@@ -96,8 +58,6 @@ const updateCourse = async (id) => {
   }
 }
 
-// updateCourse('644d884c69e1ca8919ad1571')
-
 const updateCourse2 = async (id) => {
   const result = await Course.findByIdAndUpdate(id, {
     $set: {
@@ -108,12 +68,12 @@ const updateCourse2 = async (id) => {
 
   console.log(result);
 }
-// updateCourse2('644d884c69e1ca8919ad1571');
 
 const removeCourse = async (id) => {
   const result = await Course.findByIdAndRemove(id);
 
   console.log(result);
 }
+
 removeCourse('644d884c69e1ca8919ad1571');
 
